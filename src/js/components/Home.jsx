84307659a44ea3743759ca61d048628dd21256b7
Luis Miguel RC312004 <luismiguelrc312004@gmail.com>
2026-03-16 11:14:36 +0000

    Pending changes exported from your codespace

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,22 +1,109 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
 import AgregarTareas from "./AgregarTareas";
 import ListaTareas from "./ListaTareas";
 import TotalTareas from "./TotalTareas";
-import "./Home.css"
+import "./Home.css";
+
+const usuario = "alesanchezr";
+const URL = "https://playground.4geeks.com/todo";
 
 const Home = () => {
-	
-  const [tareaPendiente, setTareaPendiente] = useState([]);
+
+	const [tareaPendiente, setTareaPendiente] = useState([]);
+
+	// CREAR USUARIO
+	const crearUsuario = () => {
+		fetch(`${URL}/users/${usuario}`, {
+			method: "POST"
+		})
+		.then(resp => resp.json())
+		.then(data => console.log("Usuario creado:", data))
+		.catch(error => console.log(error));
+	};
+
+	// OBTENER TAREAS
+	const obtenerTareas = () => {
+		fetch(`${URL}/users/${usuario}`)
+		.then(resp => resp.json())
+		.then(data => {
+			setTareaPendiente(data.todos);
+		})
+		.catch(error => console.log(error));
+	};
+
+	// AGREGAR TAREA
+	const agregarTarea = (texto) => {
+
+		const nuevaTarea = {
+			label: texto,
+			is_done: false
+		};
+
+		fetch(`${URL}/todos/${usuario}`, {
+			method: "POST",
+			body: JSON.stringify(nuevaTarea),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+		.then(resp => resp.json())
+		.then(() => obtenerTareas())
+		.catch(error => console.log(error));
+	};
+
+	// ELIMINAR TAREA
+	const eliminarTarea = (id) => {
+
+		fetch(`${URL}/todos/${id}`, {
+			method: "DELETE"
+		})
+		.then(() => obtenerTareas())
+		.catch(error => console.log(error));
+	};
+
+	// LIMPIAR TODAS
+	const limpiarTareas = () => {
+
+		fetch(`${URL}/users/${usuario}`, {
+			method: "DELETE"
+		})
+		.then(() => {
+			setTareaPendiente([]);
+			crearUsuario();
+		})
+		.catch(error => console.log(error));
+	};
+
+	// CARGAR AL INICIAR
+	useEffect(() => {
+		crearUsuario();
+		obtenerTareas();
+	}, []);
 
 	return (
 		<>
 			<div className="container">
+
 				<div className='title'>
 					<h1>Lista de Tareas</h1>
 				</div>
-				<AgregarTareas tareaPendiente={tareaPendiente} funcionalidad={setTareaPendiente}/>
-				<ListaTareas tareaPendiente={tareaPendiente} funcionalidad={setTareaPendiente}/>
+
+				<AgregarTareas agregarTarea={agregarTarea}/>
+
+				<ListaTareas 
+					tareaPendiente={tareaPendiente}
+					eliminarTarea={eliminarTarea}
+				/>
+
 				<TotalTareas tareaPendiente={tareaPendiente}/>
+
+				<button 
+					className="btn btn-info mt-3 mb-3"
+					onClick={limpiarTareas}
+				>
+					Limpiar todas las tareas
+				</button>
+
 			</div>
 		</>
 	);
--- src/js/components/TotalTareas.jsx
+++ src/js/components/TotalTareas.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-function TotalTareas({tareaPendiente}) {
-    return (
-        <div>
-            {tareaPendiente.length === 0 ? ("No hay tareas pendientes") : (tareaPendiente.length + " Tareas pendientes")}
-        </div>
-    )
+function TotalTareas({ tareaPendiente }) {
+
+	return (
+		<div>
+			{tareaPendiente.length === 0
+				? "No hay tareas pendientes"
+				: tareaPendiente.length + " tareas pendientes"}
+		</div>
+	)
 }
 
 export default TotalTareas
